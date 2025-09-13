32d7b4e7f7addef3aec01463e005e71c5a72a877
GUSTAVO JOAQUIM FABIAM <gustavo_fabiam@estudante.sesisenai.org.br>
2025-09-12 21:15:47 -0300

    Atividades 12/09/2025

---
+++ atividade3.py
@@ -0,0 +1,17 @@
+class Aluno():
+    def __init__(self, nome, nota):
+        self.nome = nome
+        self.nota = nota
+        
+    def verificar_aprovacao(self):
+        if self.nota >= 6:
+            return f"O aluno {self.nome} foi aprovado com média {self.nota:.2f}."
+        else:
+            return f"O aluno {self.nome} foi reprovado com média {self.nota:.2f}."
+        
+a1 = Aluno("Gustavo", 6)
+print(a1.verificar_aprovacao())
+
+a2 = Aluno("Adrian", 8)
+print(a2.verificar_aprovacao())
+#🏄‍♂️
